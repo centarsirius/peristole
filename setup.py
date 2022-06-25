a5853f11974b527945616b9ba995d194fb139fb0
centarsirius <tssachin.venkatesh@gmail.com>
2022-06-26 03:42:02 +0530

    standardized

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PERISTOLE',
+    name='peristole',
     version='0.1.0',
     license="MIT",
-    url='https://github.com/centarsirius/PERISTOLE',
+    url='https://github.com/centarsirius/peristole',
     descriptions="PERISTOLE : PackagE that geneRates tIme delay plotS due To gravitatiOnaL lEnsing",
     packages=find_packages(),
     authors='Sachin Venkatesh, Gaurava Pundir'

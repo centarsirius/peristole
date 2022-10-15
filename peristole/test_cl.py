bcd4946fea5dec518cf60d7907f1d4ca1345c1cf
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-15 20:02:13 +0530

    minor edits

---
+++ peristole/test_cl.py
@@ -0,0 +1,22 @@
+
+class pulsar:
+
+	# The init method or constructor
+	def __init__(self, a):
+
+		# Instance Variable
+		self.breed = breed
+
+	# Adds an instance variable
+	def setColor(self, color):
+		self.color = color
+
+	# Retrieves instance variable
+	def getColor(self):
+		return self.color
+
+
+# Driver Code
+Rodger = Dog("pug")
+Rodger.setColor("brown")
+print(Rodger.getColor())

22d8bce8add47f99423d4e9247ac06e3a2a5f01b
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-17 05:01:21 +0530

    check

--- peristole/test_cl.py
+++ peristole/test_cl.py
@@ -56,46 +56,46 @@ class pulsar:
 def details(pulsar):
     print(pulsar.mass, pulsar.axis, pulsar.ecc, pulsar.omega, pulsar.angle, pulsar.eta, pulsar.zeta)
     
-# def amp_plot_demo(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25, flag=0): 
-#     """
-#     Provides amplification factor for the images plotted as a function of 
-#     longitude.
-#     The user has to provide information/parameters about 
-#     the double pulsar system in the following order - semi 
-#     major axis, eccentricity, the longitude of 
-#     periastron in radians, the inclination angle of the orbital plane
-#     in degrees, the mass of the companion pulsar, and a variable 'flag' 
-#     which shows the plot for the dominant image if left at 
-#     its default value 0 and shows the subdominant case if 
-#     set to 1  
-#     """
+def amp_plot_demo(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25, flag=0): 
+    """
+    Provides amplification factor for the images plotted as a function of 
+    longitude.
+    The user has to provide information/parameters about 
+    the double pulsar system in the following order - semi 
+    major axis, eccentricity, the longitude of 
+    periastron in radians, the inclination angle of the orbital plane
+    in degrees, the mass of the companion pulsar, and a variable 'flag' 
+    which shows the plot for the dominant image if left at 
+    its default value 0 and shows the subdominant case if 
+    set to 1  
+    """
 
-#     phi = psi_vals - np.radians(omega)*np.ones(len(psi_vals))
-#     r = a*(1-e**2)/(np.ones(len(phi))+e*np.cos(phi))
-#     R_g = 2*G*M_c*M_0/c**2
-#     amplification = np.zeros((len(i), len(psi_vals)))
-#     for j in range(len(i)):
-#         R = r*(1-(np.sin(np.radians(i[j])))**2*(np.sin(psi_vals))**2)**0.5
-#         a_pll = a*np.sin(np.radians(i[j]))*(1-e**2)/(1+e*np.sin(np.radians(omega)))
-#         R_E = (2*R_g*a_pll)**0.5
-#         u = R/R_E
-#         if flag == 1:
-#             amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)-0.5  # dominant
-#         else:
-#             amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)+0.5  # subdominant
-#         plt.plot(np.degrees(psi_vals), amplification[j,:])
+    phi = psi_vals - np.radians(omega)*np.ones(len(psi_vals))
+    r = a*(1-e**2)/(np.ones(len(phi))+e*np.cos(phi))
+    R_g = 2*G*M_c*M_0/c**2
+    amplification = np.zeros((len(i), len(psi_vals)))
+    for j in range(len(i)):
+        R = r*(1-(np.sin(np.radians(i[j])))**2*(np.sin(psi_vals))**2)**0.5
+        a_pll = a*np.sin(np.radians(i[j]))*(1-e**2)/(1+e*np.sin(np.radians(omega)))
+        R_E = (2*R_g*a_pll)**0.5
+        u = R/R_E
+        if flag == 1:
+            amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)-0.5  # dominant
+        else:
+            amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)+0.5  # subdominant
+        plt.plot(np.degrees(psi_vals), amplification[j,:])
     
-#     plt.xlim(89,91)
-#     plt.xlabel('$Longitude \quad (degree)$', fontsize=15)
-#     if flag == 1:
-#         plt.yscale('log')
-#         plt.title('Sub-dominant image')
-#         plt.ylabel("$A_{-}$")
-#     else:
-#         plt.title('Dominant image')
-#         plt.ylabel("$A_{+}$")
-#     plt.legend(i)
-#     plt.show()
+    plt.xlim(89,91)
+    plt.xlabel('$Longitude \quad (degree)$', fontsize=15)
+    if flag == 1:
+        plt.yscale('log')
+        plt.title('Sub-dominant image')
+        plt.ylabel("$A_{-}$")
+    else:
+        plt.title('Dominant image')
+        plt.ylabel("$A_{+}$")
+    plt.legend(i)
+    plt.show()
 
 
 def amp_plot(pulsar, flag=0): 

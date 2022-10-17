563bb8577c18a6409eb6c3a9f34fe128302e85f4
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-18 03:57:49 +0530

    edits

--- peristole/amp.py
+++ peristole/amp.py
@@ -9,7 +9,7 @@ M_0 = 1.989e30 # mass of the sun
 psi_vals = np.linspace(np.radians(89), np.radians(91), 100)
 # the true anomaly measured from the ascending node of the pulsar
 
-def amp_plot(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25, flag=0): 
+def amp_plot(pulsar, flag=0): 
     """
     Provides amplification factor for the images plotted as a function of 
     longitude.
@@ -22,14 +22,14 @@ def amp_plot(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25,
     its default value 0 and shows the subdominant case if 
     set to 1  
     """
-
-    phi = psi_vals - np.radians(omega)*np.ones(len(psi_vals))
-    r = a*(1-e**2)/(np.ones(len(phi))+e*np.cos(phi))
-    R_g = 2*G*M_c*M_0/c**2
-    amplification = np.zeros((len(i), len(psi_vals)))
-    for j in range(len(i)):
-        R = r*(1-(np.sin(np.radians(i[j])))**2*(np.sin(psi_vals))**2)**0.5
-        a_pll = a*np.sin(np.radians(i[j]))*(1-e**2)/(1+e*np.sin(np.radians(omega)))
+    
+    phi = psi_vals - np.radians(pulsar.omega)*np.ones(len(psi_vals))
+    r = pulsar.axis*(1-pulsar.ecc**2)/(np.ones(len(phi))+pulsar.ecc*np.cos(phi))
+    R_g = 2*G*pulsar.mass*M_0/c**2
+    amplification = np.zeros((len(pulsar.angle), len(psi_vals)))
+    for j in range(len(pulsar.angle)):
+        R = r*(1-(np.sin(np.radians(pulsar.angle[j])))**2*(np.sin(psi_vals))**2)**0.5
+        a_pll = pulsar.axis*np.sin(np.radians(pulsar.angle[j]))*(1-pulsar.ecc**2)/(1+pulsar.ecc*np.sin(np.radians(pulsar.omega)))
         R_E = (2*R_g*a_pll)**0.5
         u = R/R_E
         if flag == 1:
@@ -47,8 +47,49 @@ def amp_plot(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25,
     else:
         plt.title('Dominant image')
         plt.ylabel("$A_{+}$")
-    plt.legend(i)
+    plt.legend(pulsar.angle)
     plt.show()
+
+# def amp_plot(a=8.784*1e8, e=0.0878, omega=73.8, i=[90.14,90.28,90.56], M_c=1.25, flag=0): 
+#     """
+#     Provides amplification factor for the images plotted as a function of 
+#     longitude.
+#     The user has to provide information/parameters about 
+#     the double pulsar system in the following order - semi 
+#     major axis, eccentricity, the longitude of 
+#     periastron in radians, the inclination angle of the orbital plane
+#     in degrees, the mass of the companion pulsar, and a variable 'flag' 
+#     which shows the plot for the dominant image if left at 
+#     its default value 0 and shows the subdominant case if 
+#     set to 1  
+#     """
+
+#     phi = psi_vals - np.radians(omega)*np.ones(len(psi_vals))
+#     r = a*(1-e**2)/(np.ones(len(phi))+e*np.cos(phi))
+#     R_g = 2*G*M_c*M_0/c**2
+#     amplification = np.zeros((len(i), len(psi_vals)))
+#     for j in range(len(i)):
+#         R = r*(1-(np.sin(np.radians(i[j])))**2*(np.sin(psi_vals))**2)**0.5
+#         a_pll = a*np.sin(np.radians(i[j]))*(1-e**2)/(1+e*np.sin(np.radians(omega)))
+#         R_E = (2*R_g*a_pll)**0.5
+#         u = R/R_E
+#         if flag == 1:
+#             amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)-0.5  # dominant
+#         else:
+#             amplification[j,:] = (u**2+2)/(2*u*(u**2+4)**0.5)+0.5  # subdominant
+#         plt.plot(np.degrees(psi_vals), amplification[j,:])
+    
+#     plt.xlim(89,91)
+#     plt.xlabel('$Longitude \quad (degree)$', fontsize=15)
+#     if flag == 1:
+#         plt.yscale('log')
+#         plt.title('Sub-dominant image')
+#         plt.ylabel("$A_{-}$")
+#     else:
+#         plt.title('Dominant image')
+#         plt.ylabel("$A_{+}$")
+#     plt.legend(i)
+#     plt.show()
     
 
 # example call for this function
--- peristole/pulsar_class.py
+++ peristole/pulsar_class.py
@@ -1,33 +1,44 @@
 class pulsar:
-
-	# The init method or constructor
-	def __init__(self, mass):
-		self.mass=mass
-	
-	# Adds an instance variable
-	def axis(self, axis):
-		self.axis = axis
-
-	def e(self, ecc):
-		self.ecc = ecc
-
-	def omega(self, omega):
-		self.omega = omega
-
-	def angle(self, angle):
-		self.angle = angle
-
-	def spin(self, spin):
-		self.spin = spin
-
-	def eta(self, eta):
-		self.eta = eta
-
-	def zeta(self, zeta):
-		self.zeta = zeta
+    axis=8.784E8
+    ecc=0.0878
+    omega=73.8
+    period =22.7E-3 #changed time to period
+    angle=[90.14,90.28,90.56]
+    mass=1.25
+    eta=45
+    zeta=50
+    alpha=4
+    big_phi0=115
     
-	def alpha(self, alpha):
-		self.alpha = alpha
+    def __init__(self, mass):
+        self.mass=mass
+    
+    def a(self, axis):
+        self.axis = axis
 
-	def big_phi0(self, big_phi0):
-		self.big_phi0 = big_phi0
+    def e(self, ecc):
+        self.ecc = ecc
+
+    def o(self, omega):
+        self.omega = omega
+
+    def i(self, angle):
+        self.angle = angle
+
+    def t(self, period):
+        self.period = period #change time to period everywhere
+
+    def et(self, eta):
+        self.eta = eta
+
+    def zet(self, zeta):
+        self.zeta = zeta
+    
+    def alph(self, alpha):
+        self.alpha = alpha
+
+    def b_phi(self, big_phi0): #change big_phi0 to something else for user friendliness
+        self.big_phi0 = big_phi0
+
+def details(pulsar):
+    print(pulsar.mass, pulsar.axis, pulsar.ecc, pulsar.omega, pulsar.angle, pulsar.eta, pulsar.zeta)

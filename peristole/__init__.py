8ac3563a1696b181b6c3a03bef1a2a779eb9d323
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-18 01:54:55 +0530

    edits

--- peristole/__init__.py
+++ peristole/__init__.py
@@ -2,13 +2,10 @@ import os
 
 print("PERISTOLE : PackagE that geneRates tIme delay plotS due To gravitatiOnaL lEnsing")
 
-from .amp import amp_plot
-from .geom_td import delay_combined
-from .geom_td import delay_grav
-from .geom_td import delay_geom
-from .rot_td import delay_rot
-from .lat_td import  delay_lat
-from .test_cl import amp_plot_demo
-from .test_cl import amp_plot
-from .test_cl import pulsar
-from .test_cl import details
+from .amp import *
+from .geom_td import *
+from .geom_td import *
+from .geom_td import *
+from .rot_td import *
+from .lat_td import  *
+from .test_cl import *

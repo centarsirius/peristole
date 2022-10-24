7e77f25fdc64fe80e76c4dd0b8c5bff073817a8c
gauravpundir000 <90083758+gauravpundir000@users.noreply.github.com>
2022-10-24 19:34:43 +0530

    Update ca1.py

--- peristole/ca1.py
+++ peristole/ca1.py
@@ -9,25 +9,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-G = 6.674*1e-11 # in SI units 
-c = 3*1e8 # in m/s
-M_sun=1.9884099*10E30
+G = 6.674*1e-11    # in SI units 
+c = 3*1e8          # in m/s
+M_sun=1.9884099*10E30    # in SI units
 
-i=np.radians(90.56) #edge-on inclination angles
-phi=np.linspace(np.radians(89),np.radians(91),1001) # varying from 89 to 91
+i=np.radians(90.56)          # edge-on inclination angles
+phi=np.linspace(np.radians(89),np.radians(91),1001)         # varying from 89 to 91
 e=0.0878 #eccentricity
-o_peri=np.radians(73.8) #periastron
-omega_p=1/22.7 #angular frequency in micro secs
-a=8.784E7 #orbital semimajor axis
+o_peri=np.radians(73.8)       #periastron
+omega_p=1/22.7                #angular frequency in micro secs
+a=8.784E7                     #orbital semimajor axis
 M_c=1.25*M_sun
 a_bar = a*np.sin(i)*(1-e**2)/(1+e*np.sin(o_peri))
 R_g = 2*G*M_c/c**2
 R_E = (2*R_g*a)**0.5/1000
-eta=np.radians(45) #eta - angle bw orbit and spin axis projection
-zeta=np.radians(50) #zeta - angle of separation bw line of sight and spin axis
-big_phi=phi-o_peri #orbital true anomaly
+eta=np.radians(45)            #eta - angle bw orbit and spin axis projection
+zeta=np.radians(50)           #zeta - angle of separation bw line of sight and spin axis
+big_phi=phi-o_peri            #orbital true anomaly
 big_phi0=np.radians(115)
-alpha=np.radians(4) #angle between spin axis and magnetic axis
+alpha=np.radians(4)           #angle between spin axis and magnetic axis
 chi0=np.arctan((np.sin(alpha)*np.sin(big_phi0))/((np.cos(alpha)*np.sin(zeta))-(np.cos(big_phi0)*np.sin(alpha)*np.cos(zeta))))
 
 

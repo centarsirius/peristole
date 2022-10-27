a626bd1925d1773305ba27287a26f683517f27c1
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-28 05:07:07 +0530

    doc in progress

--- peristole/pulsar_class.py
+++ peristole/pulsar_class.py
@@ -12,7 +12,7 @@ class pulsar:
         omega: Longitude of periastron (in degrees)
         period: Spin period of pulsar A (in seconds)
         angle: Orbital inclination angle (in degrees, can be a list)
-        eta: Angle between the ascending node of the orbit and the projection of the pulsar spin axis on the sky plane (in degrees)
+        eta: Angle between the ascending node of the orbit and the sky plane projection of the pulsar spin axis (in degrees)
         zeta: Angle between the pulsar spin axis and the line of sight (in degrees)
         alpha: Angle between the pulsar spin axis and magnetic axis (in degrees)
         phi_0: longitudinal projection of the magnetic axis to the plane containing the rotational axis (in degrees)

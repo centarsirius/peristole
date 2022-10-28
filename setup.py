3a92725b7e0463abf0777de4550e0884a2f1cb84
centarsirius <tssachin.venkatesh@gmail.com>
2022-10-29 03:18:31 +0530

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peristole',
-    version='1.0.9',
+    version='1.1.0',
     license="MIT",
     url='https://github.com/centarsirius/peristole',
     descriptions="PERISTOLE : PackagE that geneRates tIme delay plotS caused by graviTatiOnaL lEnsing",
